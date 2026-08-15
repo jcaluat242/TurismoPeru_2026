9b0b3e31c47c47a8ac9d5d0659736959d3a399d4
Jhon Emerson Calua Teran <jcaluat_24_2@unc.edu.pe>
2026-08-15 00:32:31 -0500

    Agregar sintaxis general de funciones

---
+++ functions/0.sintaxis.sql
@@ -0,0 +1,18 @@
+--FUNCION ESCALAR
+
+
+---crear esquema.  fn-nombre de la funcion  
+CREATE OR ALTER FUNCTION JECT.fn_TotalReservas
+(
+--parametro , tipo de dato
+@resultado int 
+)
+returns @resultado
+as
+begin
+DECLARE @contar int 
+
+		...
+	returns @contar
+end
+go
