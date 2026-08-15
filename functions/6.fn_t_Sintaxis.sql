dbf7fb091948e18967434e15a9961e14ec99e7ba
Jhon Emerson Calua Teran <jcaluat_24_2@unc.edu.pe>
2026-08-15 00:16:47 -0500

    Agregar sintaxis de funcion tabular

---
+++ functions/6.fn_t_Sintaxis.sql
@@ -0,0 +1,21 @@
+
+
+
+USE TURISMOPERU_JECT;
+GO
+
+
+--  CREATE OR ALTER FUNCTION esquema.nombre_funcion
+--  (
+ --   @Parametro1 TIPO,
+--    @Parametro2 TIPO
+--  )
+--      RETURNS TABLE
+--  AS
+--  RETURN
+--  (
+--    SELECT ...
+ --   FROM ...
+ --   WHERE ...
+--   );
+--  GO
